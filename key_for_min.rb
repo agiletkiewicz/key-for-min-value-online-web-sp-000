639dbcef248469d75a09bb4a9a9b21b0b3858c5a
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-27 21:58:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  array_for_min = []
   value = 1000 
   name_hash.each do |thing, number|
     if number < value 
       value = number
     end
   end
-  
+  name_hash.each do |thing, number|
+    if number == value 
+      min = thing 
+    end
+  end
+  min
 end
